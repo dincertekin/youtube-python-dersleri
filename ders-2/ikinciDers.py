9bb86ba9afca672aff9b1166ced8866a8e273a33
Dinçer Tekin <69431018+dincertekin@users.noreply.github.com>
2023-07-04 23:04:22 +0300

    Update ikinciDers.py

--- ders-2/ikinciDers.py
+++ ders-2/ikinciDers.py
@@ -1,1 +1,17 @@
+stringOrnegi = "Ali okula gitti."
+integerOrnegi = 5
+floatOrnegi = 10.45
+complexOrnegi = 12j
+listOrnegi = ["domates", "biber", "salatalik", "elma"]
+tupleOrnegi = ("armut", "muz", "çilek")
+dictOrnegi = {"isim": "Dinçer", "soyisim": "Tekin", "yaş": 19, "yer": "İstanbul"}
+setOrnegi = {"eleman1", "eleman2", "eleman3"}
+boolOrnegi = False
 
+print("Veri tiplerini yazdirmak icin degiskenin adini print fonksiyonu icerisine yazmamiz yeterlidir.")
+
+# Tek satirlik bir yorum.
+
+"""
+Çok satirli yorum kismi.
+"""

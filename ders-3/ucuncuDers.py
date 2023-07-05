2a22bd2ce7dc8530d043d9e59d86709d24302b08
Dinçer Tekin <69431018+dincertekin@users.noreply.github.com>
2023-07-05 19:39:22 +0300

    Create ucuncuDers.py

---
+++ ders-3/ucuncuDers.py
@@ -0,0 +1,5 @@
+print("Basit hesap makinesi")
+sayi1 = input("Toplama isleminde kullanilacak ilk sayi nedir? ")
+sayi2 = input("Toplama isleminde kullanilacak ikinci sayi nedir? ")
+toplam = int(sayi1) + int(sayi2)
+print("Toplamlari: " + str(toplam))

dee752e2ab4e4e27c4b683e53c00a24f89153829
Dinçer Tekin <dincertekin@protonmail.com>
2025-05-30 01:11:57 +0300

    Update and rename besinciDers.py to uygulama.py

--- ders-5/uygulama.py
+++ ders-5/uygulama.py
@@ -1,14 +1,12 @@
-# for nedir?
-# while nedir?
+# Döngüler, geçen ders söylediğimiz gibi fonksiyonlara benzer şekilde tekrarlayan işlemlerde kullanılır.
+# Fakat bu sefer kodun tekrarlayacağı şeylerde kullanacağız bizim tekrar yazacağımız şeylerde değil.
 
-# for ve while'ın farkı!
-
-# nerede for kullanırız?
-# nerede while kullanırız?
-
+# Örneğin 1'den 100'e kadar 1. Merhaba Dünya 2. Merhaba Dünya şeklinde yazsın istiyoruz.
 for i in range(1, 101):
+    # i adında verimiz aralıkta(1, 101) Neden mi 101? çünkü < 101 şeklinde işleyecek.
     print(str(i)+". Merhaba Dünya")
 
+# Arabalar dizisi oluşturalım.
 arabalar = [
     "Audi",
     "Mercedes",
@@ -17,18 +15,20 @@ arabalar = [
     "Bilinmeyen"
 ]
 
+# Arabalar dizisi içindeki her bir araba markasını ekrana yazdırmak istersek:
 for arabaMarkasi in arabalar:
     print(arabaMarkasi)
 
+# Infinite loop (Sonsuz Döngü) bunun sonu yok! Tehlikelidir.
 while 1 == 1:
     print('test')
 
 """
-DİĞER KISIM:
+Geçen sefer yaptığımızdan daha gelişmiş bir hesap makinesi örneği aşağıda:
 """
 
-sureklidongu = 1
-while sureklidongu == 1:
+sonsuzDongu = 1
+while sonsuzDongu == 1:
     print("---------------------------")
     print("Gelismis Hesap Makinesi")
     print(" ")

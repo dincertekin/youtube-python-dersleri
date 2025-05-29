c5e106640d03e56dbdd0489166267497c17cd9d0
Dinçer Tekin <dincertekin@protonmail.com>
2025-05-30 01:16:22 +0300

    Update and rename yedinciDers.py to uygulama.py

--- ders-7/uygulama.py
+++ ders-7/uygulama.py
@@ -1,45 +1,46 @@
-# Ice Aktaralim
+# random: Rastgele sayılar üreteceğimiz için içe aktaralım.
 import random
-import datetime
 
-# DateTime Modulu
+# Rastgele bir sayı oluşturup ekrana yazdırmak için:
 rastgeleInteger = random.randint(5, 15)
 print(rastgeleInteger)
 
 deneme = random.randrange(12, 49)
 print(deneme)
 
-# Random Modulu
 rastgeleInteger = random.randint(5, 15)
 print(rastgeleInteger)
 
 deneme = random.randrange(12, 49)
 print(deneme)
 
+# Meyveler dizisi oluşturalım.
 meyveler = [
     'elma',
     'armut',
     'muz',
     'cilek'
 ]
-
+# Rastgele bir meyve seçmesini istesek?
 print(random.choice(meyveler))
 
-# Hata Yakalama
+# Hataları ağaçtan düşen elmayı yakalar gibi yakalayabiliriz, bakınız:
 birString = "Deneme yazisi."
 birSayi = 19
 ikinciSayi = 249
 
+# Hatayı yakalarsak eğer, kodun yürütülmesine engel olmadan istediğimiz yönü verebiliriz.
 try:
     a = birString + birSayi + ikinciSayi
     print(a)
 except TypeError:
-    print('orada bir yazi sorunu var. satir 10')
+    print('Bir yazi sorunu var: Satir 10.')
 else:
     print('Baska bir durum soz konusu.')
 finally:
     print('Kesinlikle yurutulecek')
 
+# Kendi hatamızı oluşturalım:
 x = 5
 if x < 6:
   raise Exception("Alti sayisinin altinda bir rakam veremezsin.")

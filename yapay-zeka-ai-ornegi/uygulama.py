3128857550ed816febbbea2cd25fd89b017176ab
Dinçer Tekin <dincertekin@protonmail.com>
2025-05-30 01:21:39 +0300

    Update uygulama.py

--- yapay-zeka-ai-ornegi/uygulama.py
+++ yapay-zeka-ai-ornegi/uygulama.py
@@ -1,27 +1,33 @@
 import json
 from difflib import get_close_matches as yakin_sonuclari_getir
 
+# Veritabanı dosyamızı yükleyelim.
 def veritabanini_yukle():
     with open('C:\\Users\\User\\Desktop\\akilli-chatbot\\veritabani.json', 'r') as dosya:
         return json.load(dosya)
 
+# Veritabanına yeni verileri yazalım.
 def veritabanina_yaz(veriler):
     with open('C:\\Users\\User\\Desktop\\akilli-chatbot\\veritabani.json', 'w') as dosya:
         json.dump(veriler, dosya, indent=2)
 
+# Eşleşen sonuç bulma fonksiyonumuz.
 def yakin_sonuc_bul(soru, sorular):
     eslesen = yakin_sonuclari_getir(soru, sorular, n=1, cutoff=0.6)
     return eslesen[0] if eslesen else None
 
+# Soru sorulduysa cevabına bir bakalım.
 def cevabini_bul(soru, veritabani):
     for soru_cevaplar in veritabani["sorular"]:
         if soru_cevaplar["soru"] == soru:
             return soru_cevaplar["cevap"]
     return None
 
+# Ana fonksiyonumuz burası:
 def chat_bot():
     veritabani = veritabanini_yukle()
 
+    # Sonsuz döngü oluşturduk, Siz: şeklinde sürekli soracak. Kullanıcı çık yazana kadar.
     while True:
         soru = input("Siz: ")
         if soru == 'çık':
